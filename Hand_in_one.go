2d3eaaf7bc8aa62642eecab92c5be7fa36f78916
BastianGram <bapo@itu.dk>
2024-09-09 11:39:12 +0200

    Left or right handend
    Uses modulo to make every other prefer the right fork and every other prefer the left fork

--- Hand_in_one.go
+++ Hand_in_one.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-var arbiter sync.Mutex
-var arbiter2 sync.Mutex
 var chans [5]chan int
 var amountDoneEating int
 
 func main() {
 	for i := range chans {
-		chans[i] = make(chan int, 2)
+		chans[i] = make(chan int, 1)
 		chans[i] <- 0 // 0 means available, 1 means in use
 	}
 
@@ -31,44 +28,49 @@ func main() {
 
 	for amountDoneEating < 6 {
 		if amountDoneEating >= 5 {
-			fmt.Println("Terminate")
+			fmt.Println("Everyone has now eaten 3 or more times")
 			return
 		}
 	}
-
 }
 
 func filo(forkNum1 int, forkNum2 int, filoNum int) {
 	var timesEaten int = 0
 	for {
-		left := <-chans[forkNum1]
-		right := <-chans[forkNum2]
+		left := 0
+		right := 0
+		if filoNum%2 == 1 {
+			left = <-chans[forkNum1]
+			right = <-chans[forkNum2]
+		} else {
+			right = <-chans[forkNum2]
+			left = <-chans[forkNum1]
+		}
 
 		if left == 0 && right == 0 {
 			chans[forkNum1] <- 1
 			chans[forkNum2] <- 1
 			timesEaten++
 			fmt.Println("Philosopher", filoNum, "is eating")
-			time.Sleep(5 * time.Second)
+			time.Sleep(50 * time.Millisecond)
+
+			//Checks if the phiolsopher has eaten 3 times
 			if timesEaten == 3 {
 				amountDoneEating++
 			}
+
 			fmt.Println("Philosopher", filoNum, "is thinking")
-			time.Sleep(3 * time.Second)
-		} else {
-			chans[forkNum1] <- left
-			chans[forkNum2] <- right
+			time.Sleep(50 * time.Millisecond)
 		}
+		chans[forkNum1] <- left
+		chans[forkNum2] <- right
 	}
 }
 
 func fork(forkNum int) {
 	for {
 		x := <-chans[forkNum]
-		if x != 0 {
-			time.Sleep(6 * time.Second)
-			chans[forkNum] <- 0
-		} else {
+		if x == 0 {
 			chans[forkNum] <- 0
 		}
 	}
