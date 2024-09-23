1084725b08187f4f366f8327b7a742f7c1942170
Oliver Brinch <olibr@itu.dk>
2024-09-23 14:38:25 +0200

    deleted old version added readme

--- Hand_in_two_singlech/Hand_in_two_singlechannel.go
+++ Hand_in_two_singlech/Hand_in_two_singlechannel.go
@@ -1,66 +1,57 @@
 package main
 
-import(
+import (
 	"fmt"
 	"time"
 )
 
 const (
-	SYN2 = "SYN"
+	SYN2     = "SYN"
 	SYN_ACK2 = "SYN-ACK"
-	ACK2 = "ACK"
+	ACK2     = "ACK"
 )
 
-type Message2 struct{
+type Message2 struct {
 	Type string
 }
 
-func main(){
+func main() {
 	ch := make(chan Message2)
-	
-    go client2(ch)
-    go server2(ch)
 
-    // Allow time for goroutines to finish
-    time.Sleep(2 * time.Second)
+	go client2(ch)
+	go server2(ch)
+
+	// Allow time for goroutines to finish
+	time.Sleep(2 * time.Second)
 }
 
 func client2(ch chan Message2) {
-	fmt.Println("Client: Sending SYN")
+	fmt.Println("Client: Sending SYN=100")
 	ch <- Message2{Type: SYN2}
 
-	
-	
+	msg := <-ch
+	if msg.Type == SYN_ACK2 {
+		fmt.Println("Client: Recieved SYN=101 ACK=300")
 
-	msg := <- ch
-	if(msg.Type == SYN_ACK2){
-		fmt.Println("Client: Recieved SYN-ACK")
-		
 	}
 
 	ch <- Message2{Type: ACK2}
-	fmt.Println("Client: Sending ACK")
-	
+	fmt.Println("Client: Sending ACK=301")
 
 }
 
 func server2(ch chan Message2) {
 	msg := <-ch
-	if (msg.Type == SYN2){
-		fmt.Println("Server: Recieved SYN")
+	if msg.Type == SYN2 {
+		fmt.Println("Server: Recieved SYN=100")
 	}
-	
-	fmt.Println("Server: Sending SYN-ACK")
+
+	fmt.Println("Server: Sending SYN=101 ACK=300")
 	ch <- Message2{Type: SYN_ACK2}
-	
-	
 
-
-	msg2 := <- ch
-	if (msg2.Type == ACK2){
-		fmt.Println("Server: Recieved ACK. Connection Established!")
+	msg2 := <-ch
+	if msg2.Type == ACK2 {
+		fmt.Println("Server: Recieved ACK=301. Connection Established!")
 	}
 
 }
-
-	
