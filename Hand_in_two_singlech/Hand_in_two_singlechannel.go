ef01140cc9716c7ebb50a7a11c09b52fd0ddd9c3
Oliver Brinch <olibr@itu.dk>
2024-09-23 14:04:23 +0200

    handintwo

---
+++ Hand_in_two_singlech/Hand_in_two_singlechannel.go
@@ -0,0 +1,64 @@
+package main
+
+import(
+	"fmt"
+	"time"
+)
+
+const (
+	SYN2 = "SYN"
+	SYN_ACK2 = "SYN-ACK"
+	ACK2 = "ACK"
+)
+
+type Message2 struct{
+	Type string
+}
+
+func main(){
+	ch := make(chan Message2)
+	
+    go client2(ch)
+    go server2(ch)
+
+    // Allow time for goroutines to finish
+    time.Sleep(2 * time.Second)
+}
+
+func client2(ch chan Message2) {
+	ch <- Message2{Type: SYN2}
+	fmt.Println("Client: Sending SYN")
+	
+
+	msg := <- ch
+	if(msg.Type == SYN_ACK2){
+		fmt.Println("Client: Recieved SYN-ACK")
+		
+	}
+
+	ch <- Message2{Type: ACK2}
+	fmt.Println("Client: Sending ACK")
+	
+
+}
+
+func server2(ch chan Message2) {
+	msg := <-ch
+	if (msg.Type == SYN2){
+		fmt.Println("Server: Recieved SYN")
+	}
+	
+
+	ch <- Message2{Type: SYN_ACK2}
+	fmt.Println("Server: Sending SYN-ACK")
+	
+
+
+	msg2 := <- ch
+	if (msg2.Type == ACK2){
+		fmt.Println("Server: Recieved ACK. Connection Established!")
+	}
+
+}
+
+	
